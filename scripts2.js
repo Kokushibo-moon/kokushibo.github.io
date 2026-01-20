d7a7d3f17c8a4268b16d730e2d4ddeaf04e6e674
Kokushibo <legoshamil@yandex.ru>
2026-01-20 19:15:43 +0300

    dir changes

---
+++ scripts2.js
@@ -0,0 +1,7 @@
+        document.querySelector('.buy-button').addEventListener('click', function() {
+            alert('Товар добавлен в корзину! Перейдите к оформлению заказа.');
+        });
+
+        document.querySelector('.ad-button').addEventListener('click', function() {
+            alert('Беспроводные наушники добавлены к вашему заказу!');
+        });
